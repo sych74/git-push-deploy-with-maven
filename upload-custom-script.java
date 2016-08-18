a26edf3430ceefb6daad7410439eab248ac0a906
Ruslan Synytsky <siruslan@gmail.com>
2016-08-18 22:30:08 +0200

    Update upload-custom-script.java

--- upload-custom-script.java
+++ upload-custom-script.java
@@ -1,11 +1,7 @@
 //@auth
 //@required('url', 'scriptName', 'scriptType')
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+    import com.hivext.api.core.utils.Transport;
 import java.util.Collection;
 import java.util.HashMap;
 import com.hivext.api.Response;
@@ -14,49 +10,10 @@ import com.hivext.api.utils.Random;
 
 
 //reading script from URL
-int READ_TIMEOUT = 30 * 1000;
-int CONNECT_TIMEOUT = 10 * 1000;
-int BUFFER_SIZE = 2048;
-
-String url = getParam("url");
-
-HttpURLConnection conn = null;
-InputStream cis = null;
-ByteArrayOutputStream out = null;
-try {
-    conn = (HttpURLConnection) new URL(url).openConnection();
-    conn.setInstanceFollowRedirects(false);
-    conn.setReadTimeout(CONNECT_TIMEOUT);
-    conn.setConnectTimeout(READ_TIMEOUT);
-
-    byte[] buff = new byte[BUFFER_SIZE];
-    cis = conn.getInputStream();
-    out = new ByteArrayOutputStream();
-    int n;
-    while ((n = cis.read(buff)) > -1) {
-        out.write(buff, 0, n);
-    }
-} finally {
-    if (cis != null) {
-        try {
-            cis.close();
-        } catch (IOException ex) {}
-    }
-    if (out != null) {
-        try {
-            out.close();
-        } catch (IOException ex) {}
-    }
-
-    if (conn != null) {
-        conn.disconnect();
-    }
-
-}
-
+String scriptBody = new Transport().get(getParam("url"));
+    
 //inject token
 String token = Random.getPswd(64);
-String scriptBody = out.toString("UTF-8").replace("${TOKEN}", token);
 
 //delete the script if it already exists
 hivext.dev.scripting.DeleteScript(getParam("scriptName"));
