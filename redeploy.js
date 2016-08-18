f2520f71a61caad63080f5305f91dd344c192e2f
Ruslan Synytsky <siruslan@gmail.com>
2016-08-18 20:22:05 +0200

    Update redeploy.js

--- redeploy.js
+++ redeploy.js
@@ -1,4 +1,8 @@
 //@url('/redeploy') 
 //@required('token', 'targetAppid', 'nodeGroup')
 
-return jelastic.env.control.RestartContainersByGroup(targetAppid, signature, nodeGroup);
+if (token == ${TOKEN}) {
+  return jelastic.env.control.RestartContainersByGroup(targetAppid, signature, nodeGroup); 
+} else {
+  return {"result": 8, "error": "wrong token"}
+}
