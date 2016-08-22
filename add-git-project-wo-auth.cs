036a6d41a08884a60373a734329f198d28308ae0
Ruslan Synytsky <siruslan@gmail.com>
2016-08-22 02:25:13 +0200

    Update add-git-project-wo-auth.cs

--- add-git-project-wo-auth.cs
+++ add-git-project-wo-auth.cs
@@ -19,7 +19,7 @@ var params = {
    autoResolveConflict: true
 }
 
-//create and update the project 
+//add and build the project 
 resp = jelastic.env.build.AddProject(params.envName, params.session, params.nodeId, params.name, params.type, params.url, params.keyId, params.login, params.password, params.env, params.context, params.branch, params.autoupdate, params.interval, params.autoResolveConflict);
 if (resp.result != 0) return resp;
 resp = jelastic.env.build.BuildProject(params.envName, params.session, params.nodeId, resp.id);
